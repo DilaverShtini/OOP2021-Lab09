79ad2f34dbd6b5f479f8aff433c00ef5ab739cc8
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2021-11-22 14:28:04 +0100

    created ConcurrentGUI

---
+++ src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
@@ -0,0 +1,97 @@
+package it.unibo.oop.lab.reactivegui02;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class ConcurrentGUI {
+    private static final long serialVersionUID = 1L;
+    private static final double WIDTH_PERC = 0.2;
+    private static final double HEIGHT_PERC = 0.1;
+    private final JLabel display = new JLabel();
+    private final JButton up = new JButton("up");
+    private final JButton down = new JButton("down");
+    private final JButton stop = new JButton("stop");
+    private int count;
+
+    /**
+     * build a new GUI
+     */
+    public ConcurrentGUI() {
+        this.count = 0;
+        final JFrame frame = new JFrame();
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+        final JPanel panel = new JPanel(new FlowLayout());
+        panel.add(display);
+        panel.add(up);
+        panel.add(down);
+        panel.add(stop); 
+        frame.setContentPane(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        
+        final Agent agent = new Agent();
+        new Thread(agent).start();        
+        
+        stop.addActionListener(new ActionListener() {
+            /**
+             * event handler associated to action event on button stop.
+             * 
+             * @param e
+             *            the action event that will be handled by this listener
+             */
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                // Agent should be final
+                agent.stopCounting();
+                up.setEnabled(false);
+                down.setEnabled(false);
+            }
+        });        
+    }
+    
+    /*
+     * The counter agent is implemented as a nested class. This makes it
+     * invisible outside and encapsulated.
+     */
+    private class Agent implements Runnable {
+        
+        private volatile boolean stop;
+
+        @Override
+        public void run() {
+            while (!this.stop) {
+                try {
+                    SwingUtilities.invokeAndWait(new Runnable() {
+                        @Override
+                        public void run() {
+                            ConcurrentGUI.this.display.setText(Integer.toString(count));
+                        }
+                    });
+                    
+                    Thread.sleep(100);
+                } catch (InvocationTargetException | InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        /**
+         * External command to stop counting.
+         */
+        public void stopCounting() {
+            this.stop = true;
+        }
+        
+    }
+}
