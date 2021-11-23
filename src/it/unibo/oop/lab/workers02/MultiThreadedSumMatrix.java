20e136d17af5830265183a12f64f8c02618ca611
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2021-11-23 09:14:50 +0100

    created class MultiThreadedSumMatrix

---
+++ src/it/unibo/oop/lab/workers02/MultiThreadedSumMatrix.java
@@ -0,0 +1,10 @@
+package it.unibo.oop.lab.workers02;
+
+public class MultiThreadedSumMatrix {
+    private final int n;
+
+    public MultiThreadedSumMatrix(final int n) {
+        this.n = n;
+    }
+    
+}
